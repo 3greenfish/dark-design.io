e27d8874e5200722a6c4dc9a3ffb33f08bb2c3e4
3greenfish <curtis.boone@gmail.com>
2025-10-25 16:57:34 -0700

    Add files via upload

---
+++ core.js
@@ -0,0 +1,76 @@
+let foodValue = 2;
+
+function toggleActive(e) {
+	const targetPanelId = e.target.getAttribute('data-target');
+	const targetPanel = document.getElementById(targetPanelId);
+	const activePanels = document.getElementsByClassName('active');
+	if (activePanels) {
+		activePanels[0].classList.toggle('active'); /* hides everything */
+	} 
+	targetPanel.classList.toggle('active')
+	// wait 1 second then show selected panel
+}
+
+
+function expandButton(butt) {
+	const buttonElement = butt.target.getAttribute('data-target');
+	const targetContent = document.getElementById(buttonElement + "Content");
+	if (targetContent.style.display == "block") {
+		targetContent.style.display = "none"; /* hide content DIV */
+		document.getElementById(buttonElement).style.borderBottom = "1px solid black"; /* restore border */	
+		document.getElementById(buttonElement).style.borderRadius = "10px"; /* restore rounded corners */	
+		targetContent.style.maxHeight = "0";
+
+	} else {
+		targetContent.style.display = "block";
+		targetContent.style.maxHeight = targetContent.scrollHeight + "px";
+		document.getElementById(buttonElement).style.borderBottom = "none";
+		document.getElementById(buttonElement).style.borderRadius = "10px 10px 0 0";	
+	}
+}
+
+function postMessage(event, eventValue) {
+	const sourceButton = event;
+	/* const messageCenter = ; */
+	let messageText;
+	
+	if (sourceButton == "GatherFood") {
+		messageText = "You have gathered " + eventValue + " food. You now have " + foodValue + " food.";
+	} else {
+		messageText = "You did a thing??";
+	}
+	document.getElementById("messageCurrent").innerText = messageText;
+}
+
+function buttonClick(event, amount) {
+	const sourceButton = event.target.getAttribute('data-target');
+	const actionType = event.target.getAttribute('data-type');
+	
+	if (actionType == "gather" && sourceButton == "GatherFood") {
+		foodValue += 1;
+		postMessage(sourceButton,amount);
+		document.getElementById("foodCurrent").innerText = foodValue;
+		/* document.getElementById("GatherFoodButton").innerHTML = "<div class=\"collapsible\" data-type=\"gather\" data-target=\"GatherFood\" id=\"GatherFoodButton\" onClick=\"buttonClick(event," + foodValue + ")\">Gather Food</div>"; */
+		
+	}
+	
+	
+	
+}
+
+
+/*
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length, i++) {
+	coll[i].addEventListener("click", function() {
+		this.classList.toggle("active");
+		var content = this.nextElementSibling;
+		if (content.style.display === "block") {
+			content.style.display = "none";
+		} else {
+			content.style.display = "block";
+		}
+	});
+}*/
